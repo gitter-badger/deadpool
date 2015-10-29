edce53aee79e2bf97187118315f88a170fa2f12b
roothema <roothema@gmail.com>
2015-10-29 09:46:26 +0100

    added ExecutionProcessRepository

---
+++ testsuite-manager/src/main/java/com/deadpool/manager/repository/ExecutionProcessRepository.java
@@ -0,0 +1,10 @@
+package com.deadpool.manager.repository;
+
+import com.deadpool.manager.domain.ExecutionStrategy;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by roothema on 2015.10.29..
+ */
+public interface ExecutionProcessRepository extends CrudRepository<ExecutionStrategy, Long> {
+}
